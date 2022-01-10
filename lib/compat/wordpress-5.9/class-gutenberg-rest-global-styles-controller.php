74f8128d244d526c61fe4cffa9bed75f96cdf39c
Riad Benguella <benguella@gmail.com>
2022-01-10 10:24:57 +0100

    Retrieve the theme style variations from theme styles folder

--- lib/compat/wordpress-5.9/class-gutenberg-rest-global-styles-controller.php
+++ lib/compat/wordpress-5.9/class-gutenberg-rest-global-styles-controller.php
@@ -27,6 +27,25 @@ class Gutenberg_REST_Global_Styles_Controller extends WP_REST_Controller {
 		// List themes global styles.
 		register_rest_route(
 			$this->namespace,
+			'/' . $this->rest_base . '/themes/(?P<stylesheet>[\/\s%\w\.\(\)\[\]\@_\-]+)/variations',
+			array(
+				array(
+					'methods'             => WP_REST_Server::READABLE,
+					'callback'            => array( $this, 'get_theme_items' ),
+					'permission_callback' => array( $this, 'get_theme_items_permissions_check' ),
+					'args'                => array(
+						'stylesheet' => array(
+							'description' => __( 'The theme identifier', 'gutenberg' ),
+							'type'        => 'string',
+						),
+					),
+				),
+			)
+		);
+
+		// List themes global styles.
+		register_rest_route(
+			$this->namespace,
 			'/' . $this->rest_base . '/themes/(?P<stylesheet>[\/\s%\w\.\(\)\[\]\@_\-]+)',
 			array(
 				array(
@@ -442,6 +461,50 @@ class Gutenberg_REST_Global_Styles_Controller extends WP_REST_Controller {
 			'styles'   => $styles,
 		);
 		$response = rest_ensure_response( $result );
+
+		return $response;
+	}
+
+	/**
+	 * Checks if a given request has access to read a single theme global styles config.
+	 *
+	 * @param WP_REST_Request $request Full details about the request.
+	 * @return true|WP_Error True if the request has read access for the item, WP_Error object otherwise.
+	 */
+	public function get_theme_items_permissions_check( $request ) {
+		return $this->permissions_check( $request );
+	}
+
+	/**
+	 * Returns the given theme global styles variations.
+	 *
+	 * @param WP_REST_Request $request The request instance.
+	 *
+	 * @return WP_REST_Response|WP_Error
+	 */
+	public function get_theme_items( $request ) {
+		if ( wp_get_theme()->get_stylesheet() !== $request['stylesheet'] ) {
+			// This endpoint only supports the active theme for now.
+			return new WP_Error(
+				'rest_theme_not_found',
+				__( 'Theme not found.', 'gutenberg' ),
+				array( 'status' => 404 )
+			);
+		}
+
+		$variations     = array();
+		$base_directory = get_stylesheet_directory() . '/styles';
+		if ( file_exists( $base_directory ) ) {
+			$nested_files      = new RecursiveIteratorIterator( new RecursiveDirectoryIterator( $base_directory ) );
+			$nested_html_files = new RegexIterator( $nested_files, '/^.+\.json$/i', RecursiveRegexIterator::GET_MATCH );
+			foreach ( $nested_html_files as $path => $file ) {
+				$decoded_file = wp_json_file_decode( $path, array( 'associative' => true ) );
+				if ( is_array( $decoded_file ) ) {
+					$variations[] = $decoded_file;
+				}
+			}
+		}
+		$response = rest_ensure_response( $variations );
 
 		return $response;
 	}
